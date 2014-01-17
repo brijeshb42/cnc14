e88bf993b721f2b7b4fcbce194ccfcab09d4b1ee
Brijesh Bittu <brijeshb42@gmail.com>
2014-01-17 20:35:09 +0530

    first

---
+++ js/jquery.console.js
@@ -0,0 +1,117 @@
+/*
+Created by brijeshb42 for TV Show Manager chrome app.
+
+HTML scaffolding for creating custom confirm box.
+
+		<div class="consoleDialog">
+            <div class="overlay"></div>
+            <div class="content">
+                <div class="message">
+                	<p class=heading></p>
+                	<p class=console></p>
+                	<p class=console success></p>
+                	<p class=console error></p>
+                	<!-- Any of the above four -->
+                </div>
+                <div class="footer">
+                    <div class="controls">
+                        <button class="ok">OK</button><button class="error">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>-->
+*/
+
+(function($){
+
+	$.extend({
+		console: function(options){
+
+			var cls = "consoleDialog";
+
+			if(typeof options==='string' && options==='close'){
+				$('.'+cls).remove();
+				return;
+			}
+			
+			var defaults = {
+				heading: "Please wait...",
+				message: "Default message",
+				data: "",
+				type: "",
+				overlay: true,
+				closeText: "Close",
+				onclose: null,
+				time: 400,
+				clear: false,
+				attr: ""
+			};
+
+			var options = $.extend(defaults,options);
+
+			if(options.clear===false && $('.'+cls).length==1){
+				$p = $('<p></p>').addClass('console').addClass(options.type).html(options.message);
+				$('.'+cls).find('.heading').html(options.heading);
+				$('.'+cls).find('.message').append($p);
+				return;
+			}else if(options.clear===true && $('.'+cls).length==1){
+				$p = $('<p></p>').addClass('console').addClass(options.type).html(options.message);
+				$heading = $('<p></p>').addClass('heading').html(options.heading);
+				$('.'+cls).find('.message').html($heading).append($p);
+				return;
+			}
+
+			function dialogClosed(callback){
+				if(callback){
+					callback();
+				}				
+				$box.fadeOut(options.time,function(){
+					$(this).remove();
+				});
+			}
+
+			if($('.'+cls).length>0)
+				$('.'+cls).remove();
+
+			var $box = $('<div></div>').addClass(cls).hide();
+
+			if(options.overlay && options.overlay===true){
+				$overlay = $('<div></div>').addClass('overlay');
+				$overlay.click(function(){
+					$box.remove();
+				});
+				$box.append($overlay);
+			}
+			//.appendTo($('body'));
+			$content = $('<div></div>').addClass('content');
+			if(options.attr!=="")
+				$content.attr("style",options.attr);
+			$message = $('<div></div>').addClass('message');
+
+			$heading = $('<p></p>').addClass('heading').html(options.heading);
+			if(options.data!==""){
+				$message.append($heading);
+			}
+			else{
+				$log = $('<p></p>').addClass('console').addClass(options.type).html(options.message);
+				$message.append($heading).append($log);
+			}
+			$footer = $('<div></div>').addClass('footer');
+			$controls = $('<div></div>').addClass('controls');
+
+			$cancelBtn = $('<button></button>').addClass('error').html(options.closeText);
+			$cancelBtn.click(function(){
+				dialogClosed(options.onclose);
+			});
+			$controls.append($cancelBtn).appendTo($footer);
+			if(options.data!==""){
+				$content.append($message).append(options.data).append($footer).appendTo($box);
+			}
+			else
+				$content.append($message).append($footer).appendTo($box);
+			$box.appendTo('body').fadeIn(options.time);
+			$cancelBtn.focus();
+		}
+	});
+
+})(jQuery);
---
+++ js/main.js
@@ -0,0 +1,106 @@
+var ajax_form = true;
+
+var REG_LINK = ['https://docs.google.com/forms/d/1Ullvh-GfuCKf9kSEd4BgHAkVZ-BD0VGwJpVdCr0Bj3E/viewform',
+				'https://docs.google.com/forms/d/19IBgXR7yQlFVG0YySb3R5R7NBj6AYLhHM4zZSa5iBbU/viewform',
+				'https://docs.google.com/forms/d/1Nwd6E8gx-_MtO8F0oLqLY7NIzbOt4yI8467cLH7_FvI/viewform'];
+
+var GAMES = [
+	{
+		name: "Counter Strike 1.6",
+		data: "<p class='info'>Counter Strike needs no description.\
+			   Its the one of the most played game in the world, and battles still rages between clans\
+			   in the bid to become the best of the best.<br />Here's your chance to put to test your FPS\
+			   skills and be the best. BIT Mesra presents you CnC '14 , where clans from all over India will\
+			   be competing against each other. Loads of prizes awaits for the ultimate winner.\
+			   So, why wait - Play Hard, G0 Pr0...</p><br />\
+			  <p class='info'><a href='"+REG_LINK[0]+"' target='_blank' >Register</a></p>"
+	},
+	{
+		name: "FIFA",
+		data: "<p class='info'>Get closer to the action than ever before by mastering the skills\
+			   required to play like a real professional football player in FIFA.\
+			   Design your own unique strategy to create the perfect formation and tactics. \
+			   Set each player's exact position in the formation, and assign specific attacking \
+			   and defensive runs to unlock the opposition.</p><br />\
+			   <p class='info'><a href='"+REG_LINK[1]+"' target='_blank' >Register</a></p>"
+	},
+	{
+		name: "Need For Speed",
+		data: "<p class='info'>From the creators of the critically-acclaimed Need for Speed Underground \
+			  series, Need for Speed Most Wanted combines the thrill of street racing and tuner \
+			  customization with the intensity of police pursuits.<br />Get in the driver’s seat \
+			  of the most sought-after race cars—including tuners, exotics, muscle cars, and other \
+			  sports cars—and experience high-speed, open-world street racing at its best. Tune \
+			  your ride, win races, evade cops, become the most wanted. . . .</p><br />\
+			  <p class='info'><a href='"+REG_LINK[1]+"' target='_blank' >Register</a></p>"
+	}
+];
+
+$( document ).ready(function() { // Document ready
+ 
+/*-----------------------------------------------------------------------------------*/
+/*	01. FADE IN EFFECTS
+/*-----------------------------------------------------------------------------------*/
+		
+		$('#header .picture').hide();
+
+		$('.fade1').delay(300).fadeIn(1500);
+		
+		$('.fade2').delay(600).fadeIn(1500);
+		
+		$('.fade3').delay(900).fadeIn(1500);
+		
+		$('#header .picture').delay(800).show('fast').animate({'top': 35}, 1500, 'easeOutBack');
+			 
+		$('#header .picture').css('top', function(){ return $(this).height() });
+
+
+/*-----------------------------------------------------------------------------------*/
+/*	02. Get the CBPSCROLLER
+/*-----------------------------------------------------------------------------------*/
+
+
+			//new cbpScroller( document.getElementById( 'cbp-so-scroller' ) );
+
+			
+/*-----------------------------------------------------------------------------------*/
+/*	03. SMOOTH SCROLLING ON BUTTON
+/*-----------------------------------------------------------------------------------*/
+	
+
+$('.button').click(function(e){
+    $('html,body').scrollTo(this.hash,this.hash);
+    e.preventDefault();
+});
+
+$('.features li').click(function(e){
+	$this = $(this);
+	console.log($this);
+	var i = $this.data('arr');
+	$.console({
+		heading: GAMES[i].name,
+		message: 'Register',
+		data: GAMES[i].data
+	});
+});
+
+$("#reach-bit").click(function(){
+	$.console({
+		heading: 'Reach BIT',
+		data: '<div style="text-align:center"><iframe width="600" height="400" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://maps.google.co.in/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=Fd-EZAEdIVMWBSnfc4fXeOH0OTE5DOPVdllV5Q%3BFQdGZQEdiLYXBSEE58XzHAwYZinne8LwU_v0OTEE58XzHAwYZg&amp;q=kantatoli+to+bit+mesra&amp;sll=23.416715,85.452835&amp;sspn=0.035285,0.066047&amp;ie=UTF8&amp;ll=23.396361,85.388217&amp;spn=0.066444,0.086529&amp;t=h&amp;saddr=kantatoli&amp;daddr=bit+mesra&amp;output=embed"></iframe><br /><small><a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=Fd-EZAEdIVMWBSnfc4fXeOH0OTE5DOPVdllV5Q%3BFQdGZQEdiLYXBSEE58XzHAwYZinne8LwU_v0OTEE58XzHAwYZg&amp;q=kantatoli+to+bit+mesra&amp;sll=23.416715,85.452835&amp;sspn=0.035285,0.066047&amp;ie=UTF8&amp;ll=23.396361,85.388217&amp;spn=0.066444,0.086529&amp;t=h&amp;saddr=kantatoli&amp;daddr=bit+mesra" style="color:#0000FF;text-align:left">View Larger Map</a></small></div>'
+	});
+	return false;
+});
+
+$("#tshirt>a").click(function(){
+	$.console({
+		heading: 'CnC Tshirt',
+		data: '<p class="info"><a href="'+REG_LINK[2]+'"" target="_blank" >Get It</a></p><img width="500" src="/images/front.jpg" /><img width="500" src="/images/back.jpg" />',
+		attr: 'top:10px'
+	});
+	return false;
+});
+
+});
+
+			
